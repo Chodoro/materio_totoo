2d870d60c7e4c3f956af7bcf9162088cb12a59e2
Chodoro <teodorejames@gmail.com>
2025-05-23 23:37:13 +0800

    Fix nested git repo issue

---
+++ materio/apps/dashboards/views.py
@@ -0,0 +1,14 @@
+from django.views.generic import TemplateView
+from .models import College, Organization, OrgMember, Program, Student
+from web_project import TemplateLayout
+
+
+class DashboardsView(TemplateView):
+    def get_context_data(self, **kwargs):
+        context = TemplateLayout.init(self, super().get_context_data(**kwargs))
+        context['colleges_count'] = College.objects.count()
+        context['organizations_count'] = Organization.objects.count()
+        context['programs_count'] = Program.objects.count()
+        context['students_count'] = Student.objects.count()
+
+        return context
